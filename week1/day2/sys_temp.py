779e077e8dc25c1aac2ce893ef22c8691aa72995
Vihaan Gautam <vihaangautam04@gmail.com>
2026-07-30 16:34:11 +0530

    Initial commit

---
+++ week1/day2/sys_temp.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="I love you"
+message_systems={
+    "role":"system",
+    "content":"You are a girlfriend"
+}
+# message me role and content
+message={
+    "role": role,
+    "content": prompt
+}
+
+messages=[message]
+
+response=client.chat.completions.create(model=model, messages=messages, temparature=0.7)
+# print(response)
+
+# print("#######################################")
+
+answer=response.choices[0].message.content
+# print(answer)
